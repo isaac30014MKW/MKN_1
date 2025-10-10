1e53a42e7c81940aee2550632cbebc3d9fc73ead
isaac30014MKW <isaac30014@gmail.com>
2025-10-10 11:32:04 -0600

    Delete Pagina MKN directory

--- Pagina MKN/java.js
+++
@@ -1,9 +0,0 @@
-// Espera a que todo el contenido de la página esté cargado
-document.addEventListener("DOMContentLoaded", function() {
-  // Selecciona el elemento que quieres animar
-  const titulo = document.querySelector("h1");
-
-  // Aplica una clase CSS para iniciar la animación
-  titulo.classList.add("rebote");
-});
-
